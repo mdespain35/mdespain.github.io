24ad213857c4e681ed8012d3dae50396a40fea80
mdespain35 <mdespain35@gmail.com>
2021-05-02 13:42:18 -0700

    Updated with working Javascript

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.querySelector("#update").innerHTML = `Last Updated: ${document.lastModified}`;
+let copy = document.querySelector("#copyright").innerHTML;
+let date = new Date();
+let year = date.getFullYear();
+copy = copy.replace("Year", year);
+document.querySelector("#copyright").innerHTML = copy;
+
+document.querySelector("#update").innerHTML = `Last Modified: ${document.lastModified}`;
