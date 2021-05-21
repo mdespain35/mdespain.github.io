0a3b3f4384b98464b3f1c1abc2bf7b4802fc640e
mdespain35 <60945094+mdespain35@users.noreply.github.com>
2021-05-21 15:34:42 -0700

    Delete JS directory

--- JS/index.js
+++
@@ -1,7 +0,0 @@
-let copy = document.querySelector("#copyright").innerHTML;
-let date = new Date();
-let year = date.getFullYear();
-copy = copy.replace("Year", year);
-document.querySelector("#copyright").innerHTML = copy;
-
-document.querySelector("#update").innerHTML = `Last Modified: ${document.lastModified}`;
