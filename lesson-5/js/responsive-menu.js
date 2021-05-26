0d29d981e963fcc5d47379b67fadb14f41cd1699
mdespain35 <mdespain35@gmail.com>
2021-05-26 09:48:28 -0700

    Initial js and css for week 5

--- lesson-5/js/responsive-menu.js
+++ lesson-5/js/responsive-menu.js
@@ -1,8 +1,10 @@
 const menubutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.navigation');
+const navresponse = document.querySelector('nav');
 
 menubutton.addEventListener('click', () =>
-{mainnav.classList.toggle('responsive')}, false);
+{mainnav.classList.toggle('responsive')
+navresponse.classList.toggle('responsive')}, false);
 
 //To handle window resize while menu is open in small mode
 window.onresize = () => {if (window.innerWidth > 760) 
