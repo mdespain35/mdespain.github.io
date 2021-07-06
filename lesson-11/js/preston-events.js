4cfd4f2d93b474905c1dcfc89634da5a43946359
mdespain35 <mdespain35@gmail.com>
2021-07-06 15:22:31 -0700

    Added Events formatting for all pages

---
+++ lesson-11/js/preston-events.js
@@ -0,0 +1,21 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL).then(function (response) {
+    return response.json();
+}).then(function (jsonObject) {
+    const tinfo = jsonObject['towns'];
+    let eventDiv = document.getElementById('events');
+
+    for (let i = 0; i < tinfo.length; i++) {
+        if (tinfo[i].name == "Preston"){
+            for (t = 0; t < tinfo[i].events.length; t++){
+                console.log(tinfo[i].events[t]);
+                let p = document.createElement('p');
+                p.textContent = tinfo[i].events[t];
+                eventDiv.appendChild(p);
+            }
+            break;
+        }
+    }
+
+});
