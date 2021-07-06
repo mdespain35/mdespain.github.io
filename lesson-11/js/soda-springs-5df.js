c286f821d56e6061b0535f002cdea3a0679f7730
mdespain35 <mdespain35@gmail.com>
2021-07-06 10:23:47 -0700

    Update Soda Springs js

---
+++ lesson-11/js/soda-springs-5df.js
@@ -0,0 +1,50 @@
+const foreURL = "https://api.openweathermap.org/data/2.5/onecall?lat=42.654&lon=-111.605&exclude=current,minutely,hourly,alerts&appid=79ada910be24866f994eac62790cdc83"
+
+function getDayName(day) {
+    switch (day) {
+        case 0:
+          return "Sun";
+        case 1:
+          return "Mon";
+        case 2:
+           return "Tue";
+        case 3:
+          return "Wed";
+        case 4:
+          return "Thu";
+        case 5:
+          return "Fri";
+        case 6:
+          return "Sat";
+      }
+}
+
+fetch(foreURL).then((response) => response.json()).then((jsObject) => {
+    console.log(jsObject);
+    for(let i = 0; i < 5; i++) {
+        let day = parseFloat(jsObject.daily[i].dt) * 1000;
+        let nDay = new Date(day);
+        let card = document.createElement('div');
+        let h4 = document.createElement('h4');
+        let icon = document.createElement('img');
+        let p = document.createElement('p');
+
+        h4.textContent = getDayName(nDay.getDay());
+        card.appendChild(h4);
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.daily[i].weather[0].icon + '.png';
+        const desc = jsObject.daily[i].weather[0].description;
+        icon.setAttribute('src', imagesrc);
+        icon.setAttribute('alt', desc);
+        card.appendChild(icon);
+
+        if (jsObject.daily[i].dt > jsObject.daily[i].sunrise && jsObject.daily[i].dt < jsObject.daily[i].sunset) {
+            p.innerHTML = parseFloat(jsObject.daily[i].temp.day).toFixed(0).toString() + " &deg;F";
+        } else {
+            p.innerHTML = parseFloat(jsObject.daily[i].temp.eve).toFixed(0).toString() + " &deg;F";
+        }
+        card.appendChild(p);
+
+        document.getElementById('forecast-imgs').appendChild(card);
+    }
+});
---
+++ lesson-11/js/soda-springs-weather.js
@@ -0,0 +1,23 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=79ada910be24866f994eac62790cdc83&units=imperial";
+
+fetch(apiURL).then((response) => response.json()).then((jsObject) => {
+    document.getElementById('desc').textContent = jsObject.weather[0].main;
+    let highT = parseFloat(jsObject.main.temp_max);
+    document.getElementById('high').innerHTML = highT.toFixed(1).toString() + " &deg;F";
+
+    let currentT = parseFloat(jsObject.main.temp);
+    document.getElementById('current').innerHTML = currentT.toFixed(1).toString() + " &deg;F";
+
+    let speed = parseFloat(jsObject.wind.speed);
+
+    if ((speed > 3) && (currentT <= 50)) {
+        let chill = Math.round(35.74 + 0.6215 * currentT - 35.75 * Math.pow(speed, 0.16) + 0.4275 * currentT * Math.pow(speed, 0.16));
+    
+        document.getElementById('wind-chill').innerHTML = chill.toFixed(1).toString() + " &deg;F";
+    } else {
+        document.getElementById('wind-chill').textContent = "N/A";
+    }
+
+    document.getElementById('humid').textContent = jsObject.main.humidity + "%";
+    document.getElementById('wind-speed').textContent = speed.toFixed(1).toString() + " mph";
+});
