4c271c713aab51faac97bb35ac63664579490b8b
mdespain35 <mdespain35@gmail.com>
2021-07-06 10:32:42 -0700

    Tweak to api call

--- lesson-11/js/soda-springs-5df.js
+++ lesson-11/js/soda-springs-5df.js
@@ -1,4 +1,4 @@
-const foreURL = "https://api.openweathermap.org/data/2.5/onecall?lat=42.654&lon=-111.605&exclude=current,minutely,hourly,alerts&appid=79ada910be24866f994eac62790cdc83"
+const foreURL = "https://api.openweathermap.org/data/2.5/onecall?lat=42.654&lon=-111.605&exclude=current,minutely,hourly,alerts&appid=79ada910be24866f994eac62790cdc83&units=imperial"
 
 function getDayName(day) {
     switch (day) {
