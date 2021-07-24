b37e903fc2363f761cf7ed003594c2909bf7fc3f
mdespain35 <mdespain35@gmail.com>
2021-07-23 19:34:27 -0700

    Styling for banner/menu and current weather.

---
+++ final-project/js/index/current-weather.js
@@ -0,0 +1,21 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5391811&appid=79ada910be24866f994eac62790cdc83&units=imperial";
+
+fetch(apiURL).then((response) => response.json()).then((jsObject) => {
+
+    let weather = document.createElement('div');
+    let desc = document.createElement('p');
+    let temp = document.createElement('p');
+    let humid = document.createElement('p');
+
+    desc.textContent = jsObject.weather[0].main;
+
+    temp.innerHTML = parseFloat(jsObject.main.temp).toFixed(1).toString() + " &deg;F";
+    humid.textContent = jsObject.main.humidity + "%";
+
+    weather.appendChild(desc);
+    weather.appendChild(temp);
+    weather.appendChild(humid);
+    weather.setAttribute('class', 'current');
+
+    document.getElementById("current-weather").appendChild(weather);
+});
