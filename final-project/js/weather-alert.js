7787c950a6f0e9c215ddc8a516806d4cfdc9cd13
mdespain35 <mdespain35@gmail.com>
2021-07-24 12:19:09 -0700

    Finalized home page

--- final-project/js/weather-alert.js
+++ final-project/js/weather-alert.js
@@ -1,1 +1,8 @@
-alert('Whoa let\'s test this out!');
+const alertURL = "https://api.openweathermap.org/data/2.5/onecall?lat=32.7153&lon=-117.1573&exclude=current,minutely,hourly&appid=79ada910be24866f994eac62790cdc83&units=imperial"
+
+fetch(alertURL).then((response) => response.json()).then((jsObject) => {
+    try {
+        let message = jsObject.alerts.description;
+        alert(message);
+    } catch {}
+});
