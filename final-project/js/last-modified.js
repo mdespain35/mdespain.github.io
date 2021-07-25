f8f5f11eda5f27e77e5276ae7237a542fc0ba2ec
mdespain35 <mdespain35@gmail.com>
2021-07-25 16:07:26 -0700

    Last modified and page updates

---
+++ final-project/js/last-modified.js
@@ -0,0 +1,22 @@
+function lastModified() {
+    var modiDate = new Date(document.lastModified);
+    var showAs = (modiDate.getMonth() + 1) + "-" + modiDate.getDate() + "-" + modiDate.getFullYear();
+    return showAs
+}
+
+function GetTime() {
+    var modiDate = new Date();
+    var Seconds
+
+    if (modiDate.getSeconds() < 10) {
+        Seconds = "0" + modiDate.getSeconds();
+    } else {
+        Seconds = modiDate.getSeconds();
+    }
+
+    var modiDate = new Date();
+    var CurTime = modiDate.getHours() + ":" + modiDate.getMinutes() + ":" + Seconds
+    return CurTime
+}
+
+document.getElementById("last-modified").innerHTML = "Last Modified: " + lastModified() + " @ " + GetTime();
